0d016a2b0852191f740c965d8239b089685d88d0
anas benlemlih <anas.benlemlih@gmail.com>
2025-04-07 23:12:08 +0200

    [fix] - add the Min et Max in controller

--- src/main/java/com/kata/kata/controller/KataController.java
+++ src/main/java/com/kata/kata/controller/KataController.java
@@ -1,6 +1,8 @@
 package com.kata.kata.controller;
 
 import com.kata.kata.service.KataService;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +24,8 @@ public class KataController {
     @GetMapping("/{number}")
     public ResponseEntity<String> transformNumber(
             @PathVariable
+            @Min(value = 0, message = "Le nombre doit être supérieur ou égal à 0")
+            @Max(value = 100, message = "Le nombre doit être inférieur ou égal à 100")
             int number ){
         String result = kataService.transform(number);
         return ResponseEntity.ok(result);
